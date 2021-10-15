275004480db8f2a56a93806197aa2d6c95d34681
Nick Seddon <nickseddon187@gmail.com>
2021-10-15 19:37:46 -0400

    Deliverable 2 added.

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+-- Deliverable 1
 -- Create new table for retiring employees
 SELECT e.emp_no,
     e.first_name,
@@ -37,3 +38,26 @@ ORDER BY count DESC;
 
 -- Confirm correct table contents and configuration
 SELECT * FROM retiring_titles;
+
+
+-- Deliverable 2
+-- Create new table for employees eligible for mentorship program
+SELECT DISTINCT ON(e.emp_no) e.emp_no,
+    e.first_name,
+	e.last_name,
+	e.birth_date,
+    de.from_date,
+    de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no;
+
+-- Confirm correct table contents and configuration
+SELECT * FROM mentorship_eligibility;
