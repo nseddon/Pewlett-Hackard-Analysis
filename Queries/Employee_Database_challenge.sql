dd90e7e0b03ec5c59bed918a4ef8c5e4d15e0fb5
Nick Seddon <nickseddon187@gmail.com>
2021-10-15 22:17:25 -0400

    Additional code for Deliverable 3.

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -61,3 +61,73 @@ ORDER BY e.emp_no;
 
 -- Confirm correct table contents and configuration
 SELECT * FROM mentorship_eligibility;
+
+
+-- Deliverable 3
+-- Question 1:
+-- Create Promotion eligibility list
+SELECT DISTINCT ON(e.emp_no) e.emp_no,
+    e.first_name,
+	e.last_name,
+	e.birth_date,
+    de.from_date,
+    de.to_date,
+	t.title
+INTO promotion_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01')
+	AND (de.from_date < '1998-01-01')
+	AND (e.birth_date > '1955-12-31')
+	AND (t.title = 'Engineer' OR t.title = 'Staff' OR t.title = 'Assistant Engineer')
+ORDER BY e.emp_no;
+
+-- Confirm correct table contents and configuration
+SELECT * FROM promotion_eligibility;
+
+-- Count of each position eligible for promotion
+SELECT COUNT(pe.title) AS "count", 
+pe.title
+INTO promotion_titles
+FROM promotion_eligibility AS pe
+GROUP BY title
+ORDER BY count DESC;
+
+-- Confirm correct table contents and configuration
+SELECT * FROM promotion_titles;
+
+-- Determine the vacancy totals for each unique title after retire/promotion
+-- Show list of retiring_titles
+SELECT * FROM retiring_titles;
+
+-- Question 2:
+-- Count of mentorship eligible employees in each department by title
+SELECT me.emp_no,
+	me.title,
+	d.dept_name,
+	me.first_name,
+	me.last_name
+INTO mentorship_eligibility_by_dept
+FROM mentorship_eligibility AS me
+INNER JOIN dept_emp AS de
+	ON (me.emp_no = de.emp_no)
+INNER JOIN departments AS d
+        ON (de.dept_no = d.dept_no)
+		
+-- Confirm correct table contents and configuration
+SELECT * FROM mentorship_eligibility_by_dept;
+
+-- Count of mentorship by title by dept
+SELECT COUNT(mebd.title) AS "count", 
+mebd.title,
+mebd.dept_name
+INTO mentorship_dept_counts
+FROM mentorship_eligibility_by_dept AS mebd
+GROUP BY dept_name, title
+ORDER BY dept_name, count DESC;
+
+-- Confirm correct table contents and configuration
+SELECT * FROM mentorship_dept_counts;
